f91f5d1fcb1954d08b9729e33b60d489c11ee194
Sjpeti97 <sjpeti97@gmail.com>
2021-10-07 13:32:53 +0200

    feature: add import and export button to the ui

--- src/main/java/com/codecool/dungeoncrawl/logic/GameSaver.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/GameSaver.java
@@ -4,6 +4,7 @@ import com.codecool.dungeoncrawl.dao.GameDatabaseManager;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.SQLException;
@@ -33,6 +34,18 @@ public class GameSaver {
         try {
             FileWriter writer = new FileWriter(String.format("src/main/resources/saves/%s.txt", saveName));
             writer.write(saveContent);
+            writer.close();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void exportSave(String saveName, File location, ArrayList<GameMap> maps) throws IOException {
+        try {
+            String json = new Gson().toJson(maps);
+            FileWriter writer = new FileWriter(location+".json");
+            writer.write(json);
             writer.close();
         }
         catch (IOException e){
